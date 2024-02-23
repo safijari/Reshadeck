ebb05ea9014f49ce234f6a4ca579079358529cb2
Jariullah <safijari@isu.edu>
2024-02-23 17:57:32 -0600

    Initial work for screensaver mode

--- src/index.tsx
+++ src/index.tsx
@@ -19,6 +19,59 @@ import { VFC, useState, useEffect } from "react";
 import { MdWbShade } from "react-icons/md";
 import logo from "../assets/logo.png";
 
+
+class ReshadeckLogic
+{
+    serverAPI: ServerAPI;
+    dataTakenAt: number = Date.now();
+    // prevReading: any = {x: null, y: null, z: null};
+
+    constructor(serverAPI: ServerAPI) {
+	this.serverAPI = serverAPI;
+    }
+
+    handleButtonInput = async (val: any[]) => {
+        if (Date.now() - this.dataTakenAt < 1000) {
+            return;
+        }
+	/*
+	  R2 0
+	  L2 1
+	  R1 2
+	  R2 3
+	  Y  4
+	  B  5
+	  X  6
+	  A  7
+	  UP 8
+	  Right 9
+	  Left 10
+	  Down 11
+	  Select 12
+	  Steam 13
+	  Start 14
+	  QAM  ???
+	  L5 15
+	  R5 16*/
+        // console.log(val[0].flGravityVectorX);
+        // console.log(val[0].flGravityVectorY);
+        // console.log(val[0].flGravityVectorZ);
+        // let reading = {x: val[0].flGravityVectorX, y: val[0].flGravityVectorY, z: val[0].flGravityVectorZ};
+        // this.prevReading = reading;
+        this.dataTakenAt = Date.now();
+        // console.log(reading);
+        // console.log(val);
+        const { flSoftwareGyroDegreesPerSecondPitch, flSoftwareGyroDegreesPerSecondYaw, flSoftwareGyroDegreesPerSecondRoll } = val[0];
+        let degrees = 5;
+        if (Math.abs(flSoftwareGyroDegreesPerSecondPitch) > degrees ||
+            Math.abs(flSoftwareGyroDegreesPerSecondYaw) > degrees ||
+            Math.abs(flSoftwareGyroDegreesPerSecondRoll) > degrees) {
+            console.log('Value exceeds 20');
+            await serverAPI.callPluginMethod("set_shader", {"shader_name": "None"});
+        }
+    }
+}
+
 const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
   const baseShader = { data: "None", label: "No Shader" } as SingleDropdownOption;
   const [shader_list, set_shader_list] = useState<string[]>([]);
@@ -74,11 +127,16 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
 };
 
 export default definePlugin((serverApi: ServerAPI) => {
+  let logic = new ReshadeckLogic(serverApi);
+  let input_register = window.SteamClient.Input.RegisterForControllerStateChanges(logic.handleButtonInput);
+
   return {
-    title: <div className={staticClasses.Title}>Screentshot Aggregator</div>,
+    title: <div className={staticClasses.Title}>Reshadeck</div>,
     content: <Content serverAPI={serverApi} />,
     icon: <MdWbShade />,
     onDismount() {
+        input_register.unregister();
     },
+    alwaysRender: true
   };
 });
